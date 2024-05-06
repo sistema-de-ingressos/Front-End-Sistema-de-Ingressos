0cd612f7567c9b0676d7074a7804ec8591bea2ba
joaoarthur <joaoabritto.dev@gmail.com>
2024-05-06 09:39:09 -0300

    feat: Added Página Principal

---
+++ src/pages/PaginaPrincipal/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PaginaPrincipalContainer = styled.div`
+    width: 100%;
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 40px;
+`
