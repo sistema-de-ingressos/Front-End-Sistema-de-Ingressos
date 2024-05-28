dcc8f0c7f5cff45226d55e4745eb139326f01fcd
joaoarthur <joaoabritto.dev@gmail.com>
2024-05-28 16:41:55 -0300

    feat: Added compatibility to api payload in carrinho page

--- src/components/EventSection/EventSection.jsx
+++ src/components/EventSection/EventSection.jsx
@@ -19,77 +19,69 @@ import {
     TextWrapper,
     Title
 } from "./styles.js";
-import {useState} from "react";
+import {getDetalhesCarrinho} from "../../services/apiService.js";
+import {useQuery} from "@tanstack/react-query";
 
 export function EventSection({idEvento}) {
-    // const {data: evento, isLoading} = useQuery({
-    //     queryKey: ["evento", idEvento],
-    //     queryFn: () => getDetalhesEvento(idEvento)
-    // })
 
-    const [evento, setEvento] = useState({
-        nome: "Concerto de Verão",
-        descricao: "Lorem ipsum dolor sit amet consectetur. Diam facilisis quis nisl ac fames turpis quam sit non. Diam turpis cursus et amet cras placerat posuere. Mattis habitasse ac nam erat viverra aliquet tortor. Purus sollicitudin amet pretium phasellus augue a.",
-        local: "Teatro Municipal",
-        data: "10 de junho de 2024",
-        horario: "20:00",
-        valor: "R$20,00",
-        subtotal: "R$ 20,00",
-        taxa: "R$ 2,00",
-        total: "R$ 22,00",
-        imagem: null,
+    const {data: evento} = useQuery({
+        queryKey: ["evento", idEvento],
+        queryFn: () => getDetalhesCarrinho(idEvento)
     })
 
     return (
-        <EventoSectionContainer>
-            <CarrinhoWrapper>
-                <CarrinhoTitle>Carrinho</CarrinhoTitle>
-                <IngressoCarrinho>
-                    <Text fontSize={"12px"} fontWeight={"500"}>1x - Ingresso para {evento.nome}</Text>
-                    <Text fontSize={"14px"} fontWeight={"500"}>{evento.valor}</Text>
-                </IngressoCarrinho>
-                <AuxiliarSection>
-                    <CarrinhoLabel>Subtotal:</CarrinhoLabel>
-                    <CarrinhoText>{evento.subtotal}</CarrinhoText>
-                </AuxiliarSection>
-                <AuxiliarSection>
-                    <CarrinhoLabel>Taxa:</CarrinhoLabel>
-                    <CarrinhoText>{evento.taxa}</CarrinhoText>
-                </AuxiliarSection>
-                <AuxiliarSection>
-                    <CarrinhoTotal>Total:</CarrinhoTotal>
-                    <CarrinhoTotal>{evento.total}</CarrinhoTotal>
-                </AuxiliarSection>
-            </CarrinhoWrapper>
-            <EventoWrapper>
-                <ImagemWrapper>
-                    <Image src={evento.imagem ? `data:image/jpeg;base64,${evento.imagem}` : "http://via.placeholder.com/530x250"}/>
-                </ImagemWrapper>
-                <TextWrapper>
-                    <Title>{evento.nome}</Title>
-                    <Descricao>
-                        {evento.descricao}
-                    </Descricao>
-                    <InformationsWrapper>
-                        <Info>
-                            <InfoTitle>Local do Evento</InfoTitle>
-                            <InfoText>{evento.local}</InfoText>
-                        </Info>
-                        <Info>
-                            <InfoTitle>Data</InfoTitle>
-                            <InfoText>{evento.data}</InfoText>
-                        </Info>
-                        <Info>
-                            <InfoTitle>Horário</InfoTitle>
-                            <InfoText>{evento.horario}</InfoText>
-                        </Info>
-                        <Info>
-                            <InfoTitle>Valor</InfoTitle>
-                            <InfoText>{evento.valor}</InfoText>
-                        </Info>
-                    </InformationsWrapper>
-                </TextWrapper>
-            </EventoWrapper>
-        </EventoSectionContainer>
+        evento &&
+        (
+            <EventoSectionContainer>
+                <CarrinhoWrapper>
+                    <CarrinhoTitle>Carrinho</CarrinhoTitle>
+                    <IngressoCarrinho>
+                        <Text fontSize={"12px"} fontWeight={"500"}>1x - Ingresso para {evento.nome}</Text>
+                        <Text fontSize={"14px"} fontWeight={"500"}>{evento.valorAtual ? ` R$ ${evento.valorAtual} ` : "Gratuito"}</Text>
+                    </IngressoCarrinho>
+                    <AuxiliarSection>
+                        <CarrinhoLabel>Subtotal:</CarrinhoLabel>
+                        <CarrinhoText> {evento.valorAtual ? ` R$ ${evento.valorAtual} ` : "Gratuito"}</CarrinhoText>
+                    </AuxiliarSection>
+                    <AuxiliarSection>
+                        <CarrinhoLabel>Taxa:</CarrinhoLabel>
+                        <CarrinhoText>{evento.taxa ? ` R$ ${evento.taxa} ` : "Gratuito"}</CarrinhoText>
+                    </AuxiliarSection>
+                    <AuxiliarSection>
+                        <CarrinhoTotal>Total:</CarrinhoTotal>
+                        <CarrinhoTotal>{evento.total ? ` R$ ${evento.total} ` : "Gratuito"}</CarrinhoTotal>
+                    </AuxiliarSection>
+                </CarrinhoWrapper>
+                <EventoWrapper>
+                    <ImagemWrapper>
+                        <Image src={evento.imagemDestaque ? `data:image/jpeg;base64,${evento.imagemDestaque}` : "http://via.placeholder.com/530x250"}/>
+                    </ImagemWrapper>
+                    <TextWrapper>
+                        <Title>{evento.nomeDoEvento}</Title>
+                        <Descricao>
+                            {evento.descricao}
+                        </Descricao>
+                        <InformationsWrapper>
+                            <Info>
+                                <InfoTitle>Local do Evento</InfoTitle>
+                                <InfoText>{evento.local}</InfoText>
+                            </Info>
+                            <Info>
+                                <InfoTitle>Data</InfoTitle>
+                                <InfoText>{evento.data}</InfoText>
+                            </Info>
+                            <Info>
+                                <InfoTitle>Horário</InfoTitle>
+                                <InfoText>{evento.horario}</InfoText>
+                            </Info>
+                            <Info>
+                                <InfoTitle>Valor</InfoTitle>
+                                <InfoText>{evento.valorAtual ? ` R$ ${evento.valorAtual} ` : "Gratuito"}</InfoText>
+                            </Info>
+                        </InformationsWrapper>
+                    </TextWrapper>
+                </EventoWrapper>
+            </EventoSectionContainer>
+        )
     )
 }
