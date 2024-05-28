a013d2ceca72f677c05944046c93e1d62af4001f
joaoarthur <joaoabritto.dev@gmail.com>
2024-05-28 16:44:22 -0300

    refactor: Fixed carousel section and meus ingressos page to be compatible with api service

--- src/components/CarouselSection/CarouselSection.jsx
+++ src/components/CarouselSection/CarouselSection.jsx
@@ -1,17 +1,19 @@
 import {SectionContainer, Title} from "./styles.js";
 import {VerticalEventCard} from "../EventCard/VerticalEventCard/VerticalEventCard.jsx";
-import {useState} from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import {Autoplay, Navigation} from 'swiper/modules';
+import {useQuery} from "@tanstack/react-query";
 
 
 export function CarouselSection({title, apiService}) {
 
-    // Implementar requisição para api para receber eventos
-    const [eventos, setEventos] = useState([])
+    const {data: eventos} = useQuery({
+        queryKey: ["eventos", title],
+        queryFn: () => apiService()
+    })
 
     return (
         <SectionContainer>
@@ -38,28 +40,19 @@ export function CarouselSection({title, apiService}) {
                     },
                 }}
             >
-                {eventos.map((evento, index) =>
+                {eventos && eventos.map((evento, index) =>
                     <SwiperSlide key={index}>
-                        <VerticalEventCard data={evento.data} nome={evento.nome} id={evento.id} preco={evento.preco} local={evento.local} horario={evento.horario}/>
+                        <VerticalEventCard
+                            id={evento.id}
+                            nome={evento.nome}
+                            imagemDestaque={evento.imagemDestaque}
+                            data={evento.data}
+                            valorAtual={evento.valorAtual}
+                            local={evento.local}
+                            horario={evento.horario}
+                        />
                     </SwiperSlide>
                 )}
-
-                {/* Exemplo */}
-                <SwiperSlide>
-                    <VerticalEventCard data={"12/04"} nome={"Evento 1"} id={1} preco={"12,00"} local={"Local do Evento"} horario={"12:00"}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <VerticalEventCard data={"12/04"} nome={"Evento 2"} id={2} preco={"12,00"} local={"Local do Evento"} horario={"12:00"}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <VerticalEventCard data={"12/04"} nome={"Evento 3"} id={3} preco={"12,00"} local={"Local do Evento"} horario={"12:00"}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <VerticalEventCard data={"12/04"} nome={"Evento 4"} id={4} preco={"12,00"} local={"Local do Evento"} horario={"12:00"}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <VerticalEventCard data={"12/04"} nome={"Evento 5"} id={5} preco={"12,00"} local={"Local do Evento"} horario={"12:00"}/>
-                </SwiperSlide>
 
             </Swiper>
 
