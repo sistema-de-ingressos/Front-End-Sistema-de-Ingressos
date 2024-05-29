8a9b47d1de03e8a7379d9b6110586324bee592be
joaoarthur <joaoabritto.dev@gmail.com>
2024-05-29 01:14:21 -0300

    refactor: Added dynamic base_url to api endpoints

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 // Arquivo que possui a instância de axios que deve ser utilizada para fazer requisições para a API do Back-End
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/"
+const BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
